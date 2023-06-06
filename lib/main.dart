a157f215df8364066f403da5679ec825acea6aa4
abrinke <88974066+abrinke@users.noreply.github.com>
2023-06-06 06:43:55 +0200

    version 1.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,152 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hive/hive.dart';
+import 'package:simple_pocket/transaction.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(TransactionAdapter());
+  await Hive.openBox<Transaction>('transactions');
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const TransactionList(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class TransactionList extends StatefulWidget {
+  const TransactionList({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _TransactionListState createState() => _TransactionListState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TransactionListState extends State<TransactionList> {
+  @override
+  Widget build(BuildContext context) {
+    final transactionBox = Hive.box<Transaction>('transactions');
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('SimplePocket',
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            final transaction = Transaction(title: '', amount: '');
+            transactionBox.add(transaction);
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (_) => TransactionEditor(transaction: transaction),
+            ));
+          },
+        ),
+        body: ValueListenableBuilder(
+          valueListenable: transactionBox.listenable(),
+          builder: (BuildContext context, Box<Transaction> transactions, Widget? child) {
+            return ListView.builder(
+                itemCount: transactions.length,
+                itemBuilder: (context, index) {
+                  final transaction = transactions.getAt(index);
+
+                  return Dismissible(
+                    key: Key(transaction!.title), // Ein eindeutiger Key ist erforderlich
+                    direction: DismissDirection.endToStart, // Von rechts nach links wischen
+                    onDismissed: (direction) {
+                      // Hier können Sie die Löschlogik implementieren
+                      transaction.delete(); // Notiz löschen
+                      transactions.deleteAt(index);
+                    },
+                    background: Container(
+                      color: Colors.red, // Hintergrundfarbe beim Wischen
+                      child: Icon(Icons.delete, color: Colors.white), // Symbol beim Wischen
+                      alignment: Alignment.centerRight,
+                      padding: EdgeInsets.only(right: 20),
+                    ),
+                    child: CheckboxListTile(
+                      title: Text(transaction.title, style: TextStyle(fontSize: 20)),
+                      subtitle: Text('Amount: ${transaction.amount}'),
+                      value: transaction.isPaid,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          transaction.isPaid = value ?? false;
+                          transaction.save();
+                        });
+                      },
+                    ),
+                  );
+                }
+            );
+          },
+        )
+    );
+  }
+}
+
+class TransactionEditor extends StatefulWidget {
+  final Transaction transaction;
+
+  TransactionEditor({Key? key, required this.transaction}) : super(key: key);
+
+  @override
+  _TransactionEditorState createState() => _TransactionEditorState();
+}
+
+class _TransactionEditorState extends State<TransactionEditor> {
+  late TextEditingController _titleController;
+  late TextEditingController _amountController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _titleController = TextEditingController(text: widget.transaction.title);
+    _amountController = TextEditingController(text: widget.transaction.amount);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Edit Transaction'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            TextField(
+              controller: _titleController,
+              decoration: InputDecoration(hintText: 'Bescheibung'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            TextField(
+              controller: _amountController,
+              decoration: InputDecoration(hintText: 'Betrag'),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: Icon(Icons.save),
+        onPressed: () {
+          widget.transaction.title = _titleController.text;
+          widget.transaction.amount = _amountController.text;
+          widget.transaction.save();
+          Navigator.pop(context);
+        },
+      ),
     );
   }
 }
